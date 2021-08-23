ce534c8b4b789a5f5262ed68b3ea4b606a811099
Christopher Bergman <cb.christopherbergman@gmail.com>
2021-08-23 18:19:35 +0200

    Fixed fully working search with query.

--- src/components/Mainpage.js
+++ src/components/Mainpage.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { makeStyles } from '@material-ui/core/styles';
 import ImageList from '@material-ui/core/ImageList';
 import ImageListItem from '@material-ui/core/ImageListItem';
 import Box from '@material-ui/core/Box';
@@ -17,40 +16,56 @@ const Mainpage = (props) => {
 		setImages(data);
 	};
 
-	const useStyles = makeStyles((theme) => ({
-		root: {
-			display: 'flex',
-			flexWrap: 'wrap',
-			justifyContent: 'space-around',
-			overflow: 'hidden',
-			backgroundColor: theme.palette.background.paper,
-		},
-	}));
+	const Searchpage = async (e) => {
+		setImages(null);
+		setLoading(true);
+
+		const { data } = await axios.get(
+			`https://api.unsplash.com/search/photos/?client_id=${REACT_APP_ACCESS_KEY}&query=${e.target.value}
+				`
+		);
+		console.log(data.results);
+		setImages(data.results);
+
+		setLoading(false);
+	};
+
+	let queryFilterOnChange = (e) => {
+		console.log(e.target.value + 'this is target value');
+		setQuery(e.target.value);
+		setImages();
+
+		Searchpage(e);
+	};
 
 	useEffect(() => {
 		FirstPageLoad();
-	}, []);
+		setLoading(false);
+	}, [query.length < 1]);
 
 	const FirstPageLoad = async () => {
 		setLoading(true);
+
 		const fetchImages = async () => {
 			const { data } = await axios(
 				`https://api.unsplash.com/photos/?client_id=${REACT_APP_ACCESS_KEY}
 				`
 			);
 			savedData(data);
+			console.log(data);
 		};
 		fetchImages();
-		setLoading(false);
 	};
-
-	const classes = useStyles();
-	console.log(images);
 
 	return (
 		<div>
 			<h2>Hello</h2>
-			<Search setImages={setImages} />
+			<Search
+				setQuery={setQuery}
+				query={query}
+				Searchpage={Searchpage}
+				queryFilterOnChange={queryFilterOnChange}
+			/>
 			<Box className={'wholeImageList'}>
 				<ImageList cols={3} gap={7}>
 					{images &&
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,42 +1,35 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import Mainpage from './Mainpage';
 
 import './sass/SearchSass.scss';
 
 export default function Search(props) {
 	const [state, setState] = useState('');
-	const { REACT_APP_ACCESS_KEY } = process.env;
 
-	// useEffect(() => {
-	// 	Searchpage();
-	// }, []);
+	// const Searchpage = async (e) => {
+	// 	props.setImages('');
 
-	const Searchpage = async (e) => {
-		const fetchImages = async () => {
-			props.setImages('');
-
-			const { data } = await axios(
-				`https://api.unsplash.com/search/photos/?client_id=${REACT_APP_ACCESS_KEY}&query=${state}
-				`
-			);
-			props.setImages(data);
-		};
-		fetchImages();
-	};
+	// 	const { data } = await axios.get(
+	// 		`https://api.unsplash.com/search/photos/?client_id=${REACT_APP_ACCESS_KEY}&query=${state}
+	// 			`
+	// 	);
+	// 	console.log(data);
+	// 	props.setImages(data);
+	// };
 
 	console.log(state + 'heelo');
 	return (
-		<form className='search' onClick={Searchpage}>
+		<form className='search'>
 			<div class='search__input'>
 				<input
 					type='text'
 					name='search'
-					value={state}
-					onChange={(e) => setState(e.target.value)}
+					value={props.query}
+					onChange={props.queryFilterOnChange}
 				/>
 			</div>
 			<div className='search__buttons'>
-				<button>Search</button>
+				<button onClick={props.Searchpage}>Search</button>
 			</div>
 		</form>
 	);

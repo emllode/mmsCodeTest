09c40bfa344686524e2d27b33ff5391c7f365096
Christopher Bergman <cb.christopherbergman@gmail.com>
2021-08-24 14:57:51 +0200

    lets see if this works better without material ui icons

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import SearchIcon from '@material-ui/icons/Search';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
-import ColorLensIcon from '@material-ui/icons/ColorLens';
 import './sass/SearchSass.scss';
 
 export default function Search(props) {
@@ -27,7 +25,6 @@ export default function Search(props) {
 			{props.query < 1 ? (
 				<form className='search'>
 					<div class='search__input'>
-						<SearchIcon style={{ paddingLeft: '10px' }} />
 						<input
 							type='search'
 							name='search'
@@ -39,7 +36,6 @@ export default function Search(props) {
 			) : (
 				<form className='search'>
 					<div class='search__input'>
-						<SearchIcon style={{ paddingLeft: '10px' }} />
 						<input
 							type='search'
 							name='search'
@@ -47,15 +43,15 @@ export default function Search(props) {
 							onChange={props.queryFilterOnChange}
 						/>
 
-						<ColorLensIcon
+						<button
 							aria-controls='simple-menu'
 							aria-haspopup='true'
 							className={'colorLensIcon'}
 							onClick={handleClick}
 							style={{ paddingRight: '10px' }}
 						>
-							Open Menu
-						</ColorLensIcon>
+							color
+						</button>
 						<Menu
 							anchorEl={anchorEl}
 							keepMounted

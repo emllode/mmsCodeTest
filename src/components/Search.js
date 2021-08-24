c8ef3c5914982dd21b1d939810c212d54903cdde
Christopher Bergman <cb.christopherbergman@gmail.com>
2021-08-24 13:49:14 +0200

    removed dependinces we didn't need.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,5 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import SearchIcon from '@material-ui/icons/Search';
-import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import ColorLensIcon from '@material-ui/icons/ColorLens';
